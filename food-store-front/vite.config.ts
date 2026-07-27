306fbaccfd4067e0cd6fbb32804c8715a85786e9
Martin Carcano <149337574+MartinIgnaCarcano@users.noreply.github.com>
2026-07-27 19:46:17 -0300

    Update vite.config.ts

--- food-store-front/vite.config.ts
+++ food-store-front/vite.config.ts
@@ -13,12 +13,12 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8001',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         secure: false,
       },
       '/ws': {
-        target: 'ws://127.0.0.1:8001',
+        target: 'ws://127.0.0.1:8000',
         ws: true,
         changeOrigin: true,
         secure: false,
